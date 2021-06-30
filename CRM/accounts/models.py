77b5a63aef028d2aa4bc4e399009fe6951b89eb9
ndarvishev <ndarvishev@gmail.com>
2021-06-30 19:32:06 +0500

    Part 15

--- CRM/accounts/models.py
+++ CRM/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone =models.CharField(max_length=200, null=True)
